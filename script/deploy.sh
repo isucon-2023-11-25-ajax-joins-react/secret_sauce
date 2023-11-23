1070843c8adf08f62752a60acfef946ce0ea55c3
Ochi-Shoichi5539 <dekamintv@gmail.com>
2023-11-23 17:08:58 +0900

    デプロイスクリプトを修正する

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,10 +6,10 @@ REMOTE_HOST=$2
 
 # SSHを使用してリモートマシン上でコマンドを実行する
 ssh -i "${SSH_KEY_PATH}" ubuntu@"${REMOTE_HOST}" << 'EOF'
-cd ../isucon || exit
+sudo cd ~/
 # mainブランチにチェックアウトし、pullする
-sudo -u isucon git checkout main
-sudo -u isucon git pull
+sudo git checkout main
+sudo git pull
 # webappサービスの再起動
 sudo systemctl disable --now isucondition.ruby.service
 sudo systemctl enable --now isucondition.ruby.service
@@ -19,3 +19,4 @@ sudo systemctl restart mysql.service
 # nginxサービスの再起動（nginxを再起動するため、正しいサービス名を指定する）
 sudo systemctl restart nginx.service
 EOF
+
